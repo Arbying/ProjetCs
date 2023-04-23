23a34bdf0381bc385dfc4e49a1de67158ce44e63
Arbying <arber.banushi@student.hepl.be>
2023-04-23 15:06:01 +0200

    Ajoutez des fichiers projet.

---
+++ Modele/Personne.cs
@@ -0,0 +1,67 @@
+﻿namespace Modele
+{
+    public class Personne
+    {
+        #region Variables_membre
+        private string _nom;
+        private string _prenom;
+        private DateTime _dateNaissance;
+        #endregion
+
+        #region Constructeurs
+        // Constructeur par défaut
+        public Personne()
+        {
+            _nom = "";
+            _prenom = "";
+            _dateNaissance = DateTime.Now;
+        }
+
+        // Constructeur avec paramètres
+        public Personne(string nom, string prenom, DateTime dateNaissance)
+        {
+            _nom = nom;
+            _prenom = prenom;
+            _dateNaissance = dateNaissance;
+        }
+        #endregion
+
+        #region Accesseurs
+        public string Nom
+        {
+            get { return _nom; }
+            set { _nom = value; }
+        }
+
+        public string Prenom
+        {
+            get { return _prenom; }
+            set { _prenom = value; }
+        }
+
+        public DateTime DateNaissance
+        {
+            get { return _dateNaissance; }
+            set { _dateNaissance = value; }
+        }
+        #endregion
+
+        #region Méthodes
+        public override bool Equals(object obj)
+        {
+            if (obj == null || !(obj is Personne))
+            {
+                return false;
+            }
+
+            Personne p = (Personne)obj;
+            return _nom == p._nom && _prenom == p._prenom && _dateNaissance == p._dateNaissance;
+        }
+
+        public override string ToString()
+        {
+            return _nom + " " + _prenom + " (" + _dateNaissance.ToString("dd/MM/yyyy") + ")";
+        }
+        #endregion
+    }
+}
---
+++ Tests/Test.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Reflection;
+using Modele;
+
+namespace Test
+{
+    class Test
+    {
+        static void Main(string[] args)
+        {
+            // Création d'un objet Personne avec le constructeur par défaut
+            Modele.Personne personne1 = new Modele.Personne();
+
+            // Affichage des propriétés de l'objet Personne
+            Console.WriteLine("Personne 1 : " + personne1.ToString());
+
+            // Modification des propriétés de l'objet Personne
+            personne1.Nom = "Banushi";
+            personne1.Prenom = "Arber";
+            personne1.DateNaissance = new DateTime(1995, 3, 1);
+
+            // Affichage des propriétés de l'objet Personne après modification
+            Console.WriteLine("Personne 1 (après modification) : " + personne1.ToString());
+
+            // Création d'un autre objet Personne avec le constructeur avec paramètres
+            Modele.Personne personne2 = new Modele.Personne("Reinders", "Sebastien", new DateTime(1988, 7, 15));
+
+            // Affichage des propriétés de l'objet Personne
+            Console.WriteLine("Personne 2 : " + personne2.ToString());
+
+            // Comparaison des deux objets Personne avec la méthode Equals
+            bool isEqual = personne1.Equals(personne2);
+            Console.WriteLine("Personne 1 est égale à Personne 2 ? " + isEqual);
+
+            Console.ReadKey();
+        }
+    }
+}
